c8152e8fdf102ef29bd6e5740b1024ab19c889d6
jkwon8583 <jkwon8583@stu.d214.org>
2016-11-08 13:39:12 -0600

    Pod
    Pod File

---
+++ d214Pathways/Pods/Target Support Files/Pods-d214Pathways/Pods-d214Pathways-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_d214Pathways : NSObject
+@end
+@implementation PodsDummy_Pods_d214Pathways
+@end
---
+++ d214Pathways/Pods/Target Support Files/Pods-d214Pathways/Pods-d214Pathways-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_d214PathwaysVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_d214PathwaysVersionString[];
+
