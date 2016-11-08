ce3b9c45da7ef3624d22cdaf0d28184ebec88049
srosman8026 <srosman8026>
2016-11-08 13:49:16 -0600

    Basics
    something small

--- d214Pathways/d214Pathways/ViewController.swift
+++ d214Pathways/d214Pathways/ViewController.swift
@@ -7,19 +7,22 @@
 //
 
 import UIKit
+import SwiftyJSON
+import AlamoFire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+   
 }
 
